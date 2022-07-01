cefbd403dcb3409a1e2d4b4405f79b38bff42b92
Omkar <omkarrao5123@gmail.com>
2022-07-01 19:33:42 +0530

    project1

---
+++ Wankhede/index.js
@@ -0,0 +1,12 @@
+var index=0
+var imgList=["img1.png","img2.jfif","img3.jfif"]
+
+function change(){
+    index=index+1
+    if(index==imgList.length){
+        index=0
+    }
+    console.log(1)
+    image=document.getElementById("wkd")
+    image.src=imgList[index]
+}
